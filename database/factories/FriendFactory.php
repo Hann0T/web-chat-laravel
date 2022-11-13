f6b4d72bba375b3ffb444c2ba994dfea88e67952
Hann0T <hansolivas1@gmail.com>
2022-11-13 15:03:57 -0500

    feat: FriendFactory.

---
+++ database/factories/FriendFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Friend>
+ */
+class FriendFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'status' => 'pending',
+            'user_id' => User::factory()->create(),
+            'friend_id' => User::factory()->create(),
+        ];
+    }
+}
